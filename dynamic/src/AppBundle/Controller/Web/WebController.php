8536a1ad050faa81f592f7db277146bcb6ffd32e
Gauthier <mysterandco@gmail.com>
2018-02-21 03:35:25 +0100

    WebController run - display good

--- dynamic/src/AppBundle/Controller/Web/WebController.php
+++ dynamic/src/AppBundle/Controller/Web/WebController.php
@@ -16,34 +16,44 @@ class WebController extends Controller
      */
     public function indexAction(Benchmark $benchmark, Request $request)
     {
-        $methods = get_class_methods(new Benchmark);
+        $bench = new Benchmark();
+        $methods = get_class_methods($bench);
 
         $line = str_pad("-",38,"-");
 
         $total = 0;
 
-        $start = "<pre>$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH).
+        preg_match("#^\d.\d#", PHP_VERSION, $match);
+
+        $start = "$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH).
             "|\n$line\nStart : ".date("Y-m-d H:i:s").
-            "\nServer : {$_SERVER['SERVER_NAME']}@{}\nPHP version : ".PHP_VERSION.
+            "\nServer : xlwlx.fr".
+            "\nPHP version : ".$match[0].
             "\nPlatform : ".PHP_OS. "\n$line\n";
+
+        dump($start);
+
+        foreach ($methods as $method) {
+            if (preg_match('/^test_/', $method)) {
+                $result = $bench->$method();
+                $total += $result;
+                $content[] = str_pad($method, 25) . " : " . $result . " sec.\n";
+            }
+        }
 
         $end = str_pad("-", 38, "-") .
             "\n" . str_pad("Total time:", 25) .
             " : " . $total .
-            " sec.</pre>";
+            " sec.";
 
-        foreach ($methods as $method) {
-            if (preg_match('/^test_/', $method)) {
-                $total += $result = $method();
-                $res = str_pad($method, 25) . " : " . $result . " sec.\n";
-            }
-        }
+        dump($end);
 
 
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
             'start' => $start,
             'end' => $end,
+            'content' => $content
         ]);
     }
 }
